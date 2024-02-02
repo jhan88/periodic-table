903cc28a77a99bfd304f3b68ce564d2c21092613
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-02-02 22:30:10 +0900

    Create table with javascript

---
+++ src/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function createTable(id, rowNum, colNum, rowHead = 0, colHead = 0) {
+  const table = document.createElement('table');
+  table.setAttribute('id', id);
+  const tableBody = document.createElement('tbody');
+
+  for (let i = 0 - rowHead; i < rowNum; i++) {
+    const tableRow = document.createElement('tr');
+    tableRow.setAttribute('data-row', i);
+    for (let j = 0 - colHead; j < colNum; j++) {
+      const tableCell =
+        i < 0 || j < 0
+          ? document.createElement('th')
+          : document.createElement('td');
+      tableCell.setAttribute('data-row', i);
+      tableCell.setAttribute('data-column', j);
+      tableRow.appendChild(tableCell);
+    }
+    tableBody.appendChild(tableRow);
+  }
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+}
+createTable('periodic-table', 7, 32, 2, 1);
