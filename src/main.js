1a8445306c897ad3e7b817e7b4b81e0cd1db933f
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-02-04 00:25:01 +0900

    Assign elements to table cells

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 'use strict';
 import elements from './elements.json' assert { type: 'json' };
 
-const arrElements = elements.elements;
+const arrElements = elements.elements.slice(0, 118);
+const numPeriod = 7;
+const numGroup = 18;
+const numFblock = 14;
 
-function createTable(id, rowNum, colNum, rowHead = 0, colHead = 0) {
+const periodicTable = createTable(
+  'periodic-table',
+  numPeriod,
+  numGroup + numFblock,
+  1,
+  1
+);
+
+addPeriod(periodicTable);
+addGroup(periodicTable);
+arrElements.forEach((element) => addAtomicNum(periodicTable, element));
+
+function createTable(
+  id,
+  rowNum,
+  colNum,
+  rowHead = 0,
+  colHead = 0,
+  parent = document.body
+) {
   const table = document.createElement('table');
   table.setAttribute('id', id);
   const tableBody = document.createElement('tbody');
@@ -23,7 +45,91 @@ function createTable(id, rowNum, colNum, rowHead = 0, colHead = 0) {
     tableBody.appendChild(tableRow);
   }
   table.appendChild(tableBody);
-  document.body.appendChild(table);
+
+  return parent.appendChild(table);
 }
 
-createTable('periodic-table', 7, 32, 2, 1);
+function addPeriod(table) {
+  const listTr = table.querySelectorAll('tr');
+  const listTh = table.querySelectorAll('th');
+  const listTd = table.querySelectorAll('td');
+
+  [...listTr, ...listTh, ...listTd].forEach((cell) => {
+    const rowNum = Number(cell.dataset.row);
+    if (rowNum >= 0 && rowNum < numPeriod) {
+      cell.setAttribute('data-period', rowNum + 1);
+    }
+  });
+}
+
+function addGroup(table) {
+  const listTr = table.querySelectorAll('tr');
+  const listTh = table.querySelectorAll('th');
+  const listTd = table.querySelectorAll('td');
+
+  [...listTr, ...listTh, ...listTd].forEach((cell) => {
+    const colNum = Number(cell.dataset.column);
+    if (colNum >= 0) {
+      if (colNum < 2) {
+        cell.setAttribute('data-group', colNum + 1);
+      } else if (colNum < 2 + numFblock) {
+        cell.setAttribute('data-fblock', colNum - 1);
+      } else {
+        cell.setAttribute('data-group', colNum + 1 - numFblock);
+      }
+    }
+  });
+}
+
+function addAtomicNum(table, element) {
+  const atomicNum = element.number;
+  const period = element.period;
+  const group = element.group;
+  const isLanthanide = element.category === 'lanthanide';
+  const isActinide = element.category === 'actinide';
+
+  if (isLanthanide || isActinide) {
+    const balanceElectron = countBalanceElectronLaAc(
+      element.electron_configuration_semantic
+    );
+
+    const targetCell =
+      table.querySelector(
+        `[data-period="${period}"][data-fblock="${balanceElectron}"]`
+      ) ||
+      table.querySelector(`[data-period="${period}"][data-group="${group}"]`);
+    if (targetCell) {
+      targetCell.setAttribute('data-atomic-num', atomicNum);
+    }
+  } else {
+    const targetCell = table.querySelector(
+      `[data-period="${period}"][data-group="${group}"]`
+    );
+    if (targetCell) {
+      targetCell.setAttribute('data-atomic-num', atomicNum);
+    }
+  }
+}
+
+function countBalanceElectronLaAc(electron_configuration_semantic) {
+  const configBalance = electron_configuration_semantic
+    .split(' ')
+    .filter(
+      (config) =>
+        config.includes('f') || config.includes('d') || config.includes('p')
+    );
+
+  const balanceElectron = configBalance.reduce((previous, current) => {
+    const count = Number(
+      current
+        .replaceAll('f', '*')
+        .replaceAll('d', '*')
+        .replaceAll('p', '*')
+        .split('*')
+        .at(-1)
+    );
+    return previous + count;
+  }, 0);
+
+  return balanceElectron;
+}
