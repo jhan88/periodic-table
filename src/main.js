ad62e89c52cba2d1cfd675e8fb05a45f1c5aa896
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-03-04 12:57:36 +0900

    Create periodic table using TableBuilder

--- src/main.js
+++ src/main.js
@@ -1,91 +1,78 @@
 'use strict';
 import elements from './elements.json' assert { type: 'json' };
-import TableBuilder from './table.js';
+import { TableBuilder } from './table.js';
 
 const arrElements = elements.elements.slice(0, 118);
-const numPeriod = 7;
-const numGroup = 18;
-const numFblock = 14;
+const NUM_PERIOD = 7;
+const NUM_GROUP = 18;
+const NUM_FBLOCK = 14;
+const NUM_ELEMENT = 118;
 
-const periodicTable = new TableBuilder()
+const pTable = new TableBuilder()
   .withClassName('periodic-table')
-  .withNumRow(numPeriod)
-  .withNumCol(numGroup + numFblock)
+  .withNumRow(NUM_PERIOD)
+  .withNumCol(NUM_GROUP + NUM_FBLOCK)
   .withParent(document.querySelector('.container__periodic-table'))
   .build();
 
-addPeriod(periodicTable.table);
-addGroup(periodicTable.table);
-arrElements.forEach((element) => addAtomicNum(periodicTable.table, element));
+pTable.apply(addPeriod).toCells('th', 'td');
+pTable.apply(addGroup).toCells('th', 'td');
 
-function addPeriod(table) {
-  const listTr = table.querySelectorAll('tr');
-  const listTh = table.querySelectorAll('th');
-  const listTd = table.querySelectorAll('td');
-
-  [...listTr, ...listTh, ...listTd].forEach((cell) => {
-    const rowNum = Number(cell.dataset.row);
-    if (rowNum >= 0 && rowNum < numPeriod) {
-      cell.setAttribute('data-period', rowNum + 1);
-    }
-  });
+for (let atomNum = 1; atomNum <= NUM_ELEMENT; atomNum++) {
+  addAtomNum(atomNum);
 }
 
-function addGroup(table) {
-  const listTr = table.querySelectorAll('tr');
-  const listTh = table.querySelectorAll('th');
-  const listTd = table.querySelectorAll('td');
-
-  [...listTr, ...listTh, ...listTd].forEach((cell) => {
-    const colNum = Number(cell.dataset.col);
-    if (colNum >= 0) {
-      if (colNum < 2) {
-        cell.setAttribute('data-group', colNum + 1);
-      } else if (colNum < 2 + numFblock) {
-        cell.setAttribute('data-fblock', colNum - 1);
-      } else {
-        cell.setAttribute('data-group', colNum + 1 - numFblock);
-      }
-    }
-  });
+function addPeriod(cell) {
+  const row = Number(cell.dataset.row);
+  if (row >= 0 && row < NUM_PERIOD) {
+    cell.setAttribute('data-period', row + 1);
+  }
 }
 
-function addAtomicNum(table, element) {
-  const atomicNum = element.number;
-  const period = element.period;
-  const group = element.group;
-  const isLanthanide = element.category === 'lanthanide';
-  const isActinide = element.category === 'actinide';
-
-  if (isLanthanide || isActinide) {
-    const balanceElectron = countBalanceElectronLaAc(
-      element.electron_configuration_semantic
-    );
-
-    const targetCell =
-      table.querySelector(
-        `[data-period="${period}"][data-fblock="${balanceElectron}"]`
-      ) ||
-      table.querySelector(`[data-period="${period}"][data-group="${group}"]`);
-    if (targetCell) {
-      targetCell.setAttribute('data-atomic-num', atomicNum);
-    }
-  } else {
-    const targetCell = table.querySelector(
-      `[data-period="${period}"][data-group="${group}"]`
-    );
-    if (targetCell) {
-      targetCell.setAttribute('data-atomic-num', atomicNum);
+function addGroup(cell) {
+  const col = Number(cell.dataset.col);
+  if (col >= 0) {
+    if (col < 2) {
+      cell.setAttribute('data-group', col + 1);
+    } else if (col < 2 + NUM_FBLOCK) {
+      cell.setAttribute('data-fblock', col - 1);
+    } else {
+      cell.setAttribute('data-group', col + 1 - NUM_FBLOCK);
     }
   }
 }
 
-function countBalanceElectronLaAc(electron_configuration_semantic) {
+function addAtomNum(atomNum) {
+  const element = arrElements[atomNum - 1];
+  const period = element.period;
+  const group = element.group;
+  const fblock =
+    element.category === 'lanthanide' || element.category === 'actinide'
+      ? countBalanceElectron(element.electron_configuration_semantic) - 2
+      : 0;
+
+  pTable.cell({ period, fblock })
+    ? pTable
+        .apply((cell) => {
+          cell.setAttribute('data-atom-num', atomNum);
+        })
+        .toCell({ period, fblock })
+    : pTable
+        .apply((cell) => {
+          cell.setAttribute('data-atom-num', atomNum);
+        })
+        .toCell({ period, group });
+}
+
+function countBalanceElectron(electron_configuration_semantic) {
   const configBalance = electron_configuration_semantic
     .split(' ')
     .filter(
       (config) =>
-        config.includes('f') || config.includes('d') || config.includes('p')
+        config.includes('f') ||
+        config.includes('d') ||
+        config.includes('p') ||
+        config.includes('s')
     );
 
   const balanceElectron = configBalance.reduce((previous, current) => {
@@ -94,6 +81,7 @@ function countBalanceElectronLaAc(electron_configuration_semantic) {
         .replaceAll('f', '*')
         .replaceAll('d', '*')
         .replaceAll('p', '*')
+        .replaceAll('s', '*')
         .split('*')
         .at(-1)
     );
@@ -104,68 +92,80 @@ function countBalanceElectronLaAc(electron_configuration_semantic) {
 }
 
 // Frame Cells
-// write periods
-[...periodicTable.table.querySelectorAll('th')]
-  .filter((cell) => cell.dataset.col === '-1' && cell.dataset.period)
-  .forEach((cell) => (cell.textContent = cell.dataset.period));
+// write periods and groups
+pTable
+  .apply((cell) => {
+    if (cell.dataset.col === '-1' && cell.dataset.period) {
+      cell.textContent = cell.dataset.period;
+    } else if (cell.dataset.row === '-1' && cell.dataset.group) {
+      cell.textContent = cell.dataset.group;
+    }
+  })
+  .toCells('th');
 
-// write groups
-[...periodicTable.table.querySelectorAll('th')]
-  .filter((cell) => cell.dataset.row === '-1' && cell.dataset.group)
-  .forEach((cell) => (cell.textContent = cell.dataset.group));
+// Fill element information
+pTable
+  .apply((cell) => {
+    const atomNum = cell.dataset.atomNum;
+    if (!atomNum) {
+      return;
+    }
+    cell.classList.add('cell__info');
+    cell.innerHTML = `
+      <span class="cell__info__atom-num">${atomNum}</span>
+      <p class="cell__info__symbol">${arrElements[atomNum - 1].symbol}</p>
+    `;
+  })
+  .toCells('td');
 
-// hide unnecessary cells
-[
-  ...periodicTable.table.querySelectorAll('th'),
-  ...periodicTable.table.querySelectorAll('td'),
-]
-  .filter((cell) => cell.dataset.row < 5 && cell.dataset.fblock)
-  .forEach((cell) => cell.classList.add('cell--none'));
-
-[...periodicTable.table.querySelectorAll('td')]
-  .filter((cell) => !cell.dataset.atomicNum)
-  .forEach((cell) => cell.classList.add('cell--hidden'));
+// hide unncessary cells
+pTable
+  .apply((cell) => {
+    if (cell.dataset.row < 5 && cell.dataset.fblock) {
+      cell.classList.add('cell--none');
+    }
+  })
+  .toCells('th');
+pTable
+  .apply((cell) => {
+    if (!cell.dataset.atomNum) {
+      cell.dataset.row < 5 && cell.dataset.fblock
+        ? cell.classList.add('cell--none')
+        : cell.classList.add('cell--hidden');
+    }
+  })
+  .toCells('td');
 
 // create style div containers for f-block element cells
 const containerFblock = document.createElement('div');
-const containerLanthanide = document.createElement('div');
-const containerActinide = document.createElement('div');
-
 containerFblock.classList.add('container__fblock');
-containerLanthanide.classList.add('container__lanthanide');
-containerActinide.classList.add('container__actinide');
+pTable.table.appendChild(containerFblock);
 
-periodicTable.table.appendChild(containerFblock);
+const containerLanthanide = document.createElement('div');
+containerLanthanide.classList.add('container__lanthanide');
 containerFblock.appendChild(containerLanthanide);
+
+const containerActinide = document.createElement('div');
+containerActinide.classList.add('container__actinide');
 containerFblock.appendChild(containerActinide);
 
 // add lanthanide cells to container lanthanide
-[...periodicTable.table.querySelectorAll('td')]
-  .filter((cell) => cell.dataset.period == 6 && cell.dataset.fblock)
-  .forEach((cell) =>
-    document.querySelector('.container__lanthanide').appendChild(cell)
-  );
+pTable
+  .apply((cell) => {
+    if (cell.dataset.period === '6' && cell.dataset.fblock) {
+      containerLanthanide.appendChild(cell);
+    }
+  })
+  .toCells('.cell__info');
 
 // add actinide cells to container actinide
-[...periodicTable.table.querySelectorAll('td')]
-  .filter((cell) => cell.dataset.period == 7 && cell.dataset.fblock)
-  .forEach((cell) =>
-    document.querySelector('.container__actinide').appendChild(cell)
-  );
-
-arrElements.forEach((element) => fillInfo(element));
-
-// Fill element information
-function fillInfo(element) {
-  const targetCell = periodicTable.table.querySelector(
-    `[data-atomic-num="${element.number}"]`
-  );
-  targetCell.classList.add('cell__info');
-  targetCell.innerHTML = `
-    <span class="cell__info__atom-num">${element.number}</span>
-    <p class="cell__info__symbol">${element.symbol}</p>
-  `;
-}
+pTable
+  .apply((cell) => {
+    if (cell.dataset.period === '7' && cell.dataset.fblock) {
+      containerActinide.appendChild(cell);
+    }
+  })
+  .toCells('.cell__info');
 
 // Display element information in detail
 const sectionDisplay = document.querySelector('.container__display-info');
@@ -189,7 +189,7 @@ document.addEventListener('click', (event) => {
   }
 
   const atomicNum = event.target.dataset.atomicNum;
-  const cellFocus = periodicTable.table.querySelector('.cell__info--focus');
+  const cellFocus = pTable.table.querySelector('.cell__info--focus');
   const previousDisplay = document.querySelector('.list__display-info');
 
   cellFocus && cellFocus.classList.remove('cell__info--active');
